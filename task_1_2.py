3bf8f2e8893eb94013e3c52654d2ca12befed1c5
ikuklos <ikuklos@yandex.ru>
2021-04-04 14:19:55 +0300

    Решенное второе задание

--- task_1_2.py
+++ task_1_2.py
@@ -3,10 +3,9 @@
 cube_of_odd_numbers_list = []
 count = 0
 
-while count <= 1000:
-    if count % 2 != 0:
-        cube_of_odd_numbers_list.append(count**3)
-    count = count + 1
+for number_1 in range(1001):
+    if number_1 % 2 != 0:
+        cube_of_odd_numbers_list.append(number_1**3)
 
 print(cube_of_odd_numbers_list)
 
@@ -14,36 +13,67 @@ print(cube_of_odd_numbers_list)
 # Например, число «19 ^ 3 = 6859» будем включать в сумму, так как 6 + 8 + 5 + 9 = 28 – делится нацело на 7.
 # Внимание: использовать только арифметические операции!
 
+sum_numbers_mod_7 = 0
+for number_2 in cube_of_odd_numbers_list:
 
+    cube_list_number = number_2
+    bit_depth = 1
+    depth_length_chek = 0 < number_2 // bit_depth <= 9
+    while depth_length_chek != True:
+        bit_depth *= 10
+        depth_length_chek = 0 < number_2 // bit_depth <= 9
 
-
-sum_numbers=[]
-for num in cube_of_odd_numbers_list:
-    test_num = num
     work_list = []
-    x = 1
-    y = 0 < num // x <= 9
     count = 0
-    while not y != False:
-        x *= 10
-        y = 0 < num // x <= 9
-
-    while x >= 1:
-        work_list.append(int(num//x))
-        num -= x*work_list[count]
-        x /= 10
+    while bit_depth >= 1:
+        work_list.append(number_2 // bit_depth)
+        number_2 -= bit_depth * work_list[count]
+        bit_depth //= 10
         count += 1
 
-    sum_all_list_numbers = 0
+    sum_work_list_numbers = 0
     for number_in_work_list in work_list:
-        sum_all_list_numbers += number_in_work_list
+        sum_work_list_numbers += number_in_work_list
 
-    if sum_all_list_numbers%7 == 0:
-        sum_numbers.append(test_num)
-        sum_numbers.append(sum_all_list_numbers)
+    if sum_work_list_numbers % 7 == 0:
+        sum_numbers_mod_7 += cube_list_number
 
-print(sum_numbers)
+print(sum_numbers_mod_7) #17485588610
 
-# К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого списка,
+# К каждому элементу списка добавить 17
+
+for number_3 in range(len(cube_of_odd_numbers_list)):
+    cube_of_odd_numbers_list[number_3] += 17
+
+print(cube_of_odd_numbers_list)
+
+# и заново вычислить сумму тех чисел из этого списка,
 # сумма цифр которых делится нацело на 7.
 # Внимание: новый список не создавать!!!
+
+sum_numbers_mod_7 = 0
+for number_2 in cube_of_odd_numbers_list:
+
+    cube_list_number = number_2
+    bit_depth = 1
+    depth_length_chek = 0 < number_2 // bit_depth <= 9
+    while depth_length_chek != True:
+        bit_depth *= 10
+        depth_length_chek = 0 < number_2 // bit_depth <= 9
+
+    work_list = []
+    count = 0
+    while bit_depth >= 1:
+        work_list.append(number_2 // bit_depth)
+        number_2 -= bit_depth * work_list[count]
+        bit_depth //= 10
+        count += 1
+
+    sum_work_list_numbers = 0
+    for number_in_work_list in work_list:
+        sum_work_list_numbers += number_in_work_list
+
+    if sum_work_list_numbers % 7 == 0:
+        sum_numbers_mod_7 += cube_list_number
+
+print(sum_numbers_mod_7) #15392909930
