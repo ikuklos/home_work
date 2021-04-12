deff8e7b01a9a9f31f997e5742018479e1178b5a
macbook_pycharm <ikuklos@yandex.ru>
2021-04-12 22:00:27 +0300

    Начал делать вторую часть с сортировкой по ключам

---
+++ task_3_3.py
@@ -0,0 +1,38 @@
+# Написать функцию thesaurus(), принимающую в качестве аргументов имена сотрудников и возвращающую словарь,
+# в котором ключи — первые буквы имён, а значения — списки, содержащие имена, начинающиеся с соответствующей буквы.
+#
+# Например:
+# >>> thesaurus("Иван", "Мария", "Петр", "Илья")
+# {
+#     "И": ["Иван", "Илья"],
+#     "М": ["Мария"], "П": ["Петр"]
+# }
+
+# Подумайте: полезен ли будет вам оператор распаковки?
+
+def thesaurus(*args, key_sort = 'no'):
+    dict_thesaurus = {}
+    for word in args:
+        if word[0] not in dict_thesaurus:
+            dict_thesaurus[word[0]] = [word]
+        else:
+            dict_thesaurus[word[0]] += [word]
+    print(dict_thesaurus)
+
+
+thesaurus("Иван", "Мария", "Петр", "Илья")
+
+# Как поступить, если потребуется сортировка по ключам?
+# Можно ли использовать словарь в этом случае?
+
+def thesaurus(*args, key_sort = 'no'):
+    dict_thesaurus = {}
+    if key_sort != 'yes':
+        for word in args:
+            if word[0] not in dict_thesaurus:
+                dict_thesaurus[word[0]] = [word]
+            else:
+                dict_thesaurus[word[0]] += [word]
+    print(dict_thesaurus)
+
+thesaurus("Иван", "Мария", "Петр", "Илья")
