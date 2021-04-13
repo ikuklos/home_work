265503f00e4875599b07ccbcfdf5bed7c2716d0b
macbook_pycharm <ikuklos@yandex.ru>
2021-04-14 00:29:22 +0300

    начало второй

--- task_3_5.py
+++ task_3_5.py
@@ -6,7 +6,6 @@ nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
-print(random.choice(nouns))
 # Например:
 # >>> get_jokes(2)
 # ["лес завтра зеленый", "город вчера веселый"]
@@ -15,13 +14,42 @@ print(random.choice(nouns))
 def get_jokes(quantity):
     jokes = []
     while quantity >= 0:
-        joke = random.choice(nouns) + random.choice(adverbs) + random.choice(adjectives)
+        joke = random.choice(nouns) + ' ' + random.choice(adverbs) + ' ' + random.choice(adjectives)
         jokes.append(joke)
         quantity -= 1
     print(jokes)
 
-get_jokes(1)
-get_jokes(-1)
+get_jokes(10)
 # Документировать код функции.
 
-# Сможете ли вы добавить еще один аргумент — флаг, разрешающий или запрещающий повторы слов в шутках (когда каждое слово можно использовать только в одной шутке)? Сможете ли вы сделать аргументы именованными?
+# Сможете ли вы добавить еще один аргумент — флаг, разрешающий или запрещающий повторы слов в шутках
+# (когда каждое слово можно использовать только в одной шутке)?
+# Сможете ли вы сделать аргументы именованными?
+
+def get_jokes(quantity, repeat = 'использовать'):
+    _nouns = nouns.copy()
+    _adverbs = adverbs.copy()
+    _adjectives = adjectives.copy()
+    jokes = []
+    if repeat != 'неиспользовать':
+        while quantity >= 0:
+            noun = random.choice(_nouns)
+            adverb = random.choice(_adverbs)
+            adjective = random.choice(_adjectives)
+            joke = f'{noun} {adverb} {adjective}'
+            jokes.append(joke)
+            quantity -= 1
+    else:
+        while quantity >= 0 or len(_nouns) > 0 or len(_adverbs) > 0 or len(_adjectives) > 0:
+            noun = random.choice(_nouns)
+            adverb = random.choice(_adverbs)
+            adjective = random.choice(_adjectives)
+            _nouns.remove(noun)
+            _adverbs.remove(adverb)
+            _adjectives.remove(adjective)
+            joke = f'{noun} {adverb} {adjective}'
+            jokes.append(joke)
+            quantity -= 1
+    print(jokes)
+
+get_jokes(10, 'неиспользовать')
