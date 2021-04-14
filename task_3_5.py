4e4a0a694fb48b67322537a444e48c772e43c3a2
macbook_pycharm <ikuklos@yandex.ru>
2021-04-14 22:33:45 +0300

    финальный результат по 5-ой задаче

--- task_3_5.py
+++ task_3_5.py
@@ -20,13 +20,21 @@ def get_jokes(quantity):
     print(jokes)
 
 get_jokes(10)
+
+
 # Документировать код функции.
 
 # Сможете ли вы добавить еще один аргумент — флаг, разрешающий или запрещающий повторы слов в шутках
 # (когда каждое слово можно использовать только в одной шутке)?
 # Сможете ли вы сделать аргументы именованными?
-
-def get_jokes(quantity, repeat = 'использовать'):
+# _nouns = nouns.copy()
+# print(_nouns)
+# nun = _nouns[1]
+# _nouns.remove(nun)
+# print(_nouns)
+# print(random.choice(_nouns))
+# print(nouns[random.randrange(1,3 )])
+def get_jokes(quantity, repeat='использовать'):
     _nouns = nouns.copy()
     _adverbs = adverbs.copy()
     _adjectives = adjectives.copy()
@@ -40,16 +48,18 @@ def get_jokes(quantity, repeat = 'использовать'):
             jokes.append(joke)
             quantity -= 1
     else:
-        while quantity >= 0 or len(_nouns) > 0 or len(_adverbs) > 0 or len(_adjectives) > 0:
-            noun = random.choice(_nouns)
-            adverb = random.choice(_adverbs)
-            adjective = random.choice(_adjectives)
+        quantity = len(_nouns)
+        while quantity > 0:
+            noun = _nouns[random.randrange(0, quantity)]
+            adverb = _adverbs[random.randrange(0, quantity)]
+            adjective = _adjectives[random.randrange(0, quantity)]
             _nouns.remove(noun)
             _adverbs.remove(adverb)
             _adjectives.remove(adjective)
             joke = f'{noun} {adverb} {adjective}'
             jokes.append(joke)
             quantity -= 1
+
     print(jokes)
 
 get_jokes(10, 'неиспользовать')
