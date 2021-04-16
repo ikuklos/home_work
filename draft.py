826e26b20e58d604aca02724e3317e8437f0a614
ikuklos <ikuklos@yandex.ru>
2021-04-16 20:21:19 +0300

    Задание №2 к 4 уроку

--- draft.py
+++ draft.py
@@ -1,4 +1,8 @@
 #dictionary
-users = {'name':'Yuri', 'age':83} # {key:value}
-print(users['age'])
-print(users.get('sex', 'Нет такого поля '))
+# users = {'name':'Yuri', 'age':83} # {key:value}
+# print(users['age'])
+# print(users.get('sex', 'Нет такого поля '))
+smstr= '</CharCode>
+<Nominal>1</Nominal>
+<Name>Доллар США</Name>
+<Value>'
