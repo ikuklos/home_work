d297fcd08a1031896b1e191b6efdfc29f7ded63c
ikuklos <ikuklos@yandex.ru>
2021-04-16 21:26:18 +0300

    Задание №3 к 4 уроку. законченный вариант

--- task_4_3.py
+++ task_4_3.py
@@ -6,13 +6,13 @@
 
 import requests, datetime
 
-print(datetime.datetime(17, month04, year=2021))
 def currency_rates(char_code):
     data_of_currency = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text
     if char_code in data_of_currency:
-
+        dd_mm_yyyy = data_of_currency[data_of_currency.find('Date="')+6:data_of_currency.find('Date="')+16].split('.')
+        server_date = datetime.date(day = int(dd_mm_yyyy[0]), month = int(dd_mm_yyyy[1]), year = int(dd_mm_yyyy[2]))
         place_on_api = data_of_currency[data_of_currency.find(char_code):data_of_currency.find(char_code)+150]
-        print(f'Цена одного {char_code} =', place_on_api[place_on_api.find('</Name><Value>')+14:place_on_api.find('</Value></Valute>')], 'руб.')
+        print(f'Цена одного {char_code} =', place_on_api[place_on_api.find('</Name><Value>')+14:place_on_api.find('</Value></Valute>')], f'руб. по состоянию на {server_date}')
     else:
         print("Такая валюта не найдена.")
 
