0c1c2d7d550ad00ec7510e4eccedf30079d25311
macbook_pycharm <ikuklos@yandex.ru>
2021-05-03 22:06:24 +0300

    Закончено 1 задание 7 урока

--- task_8_1.py
+++ task_8_1.py
@@ -1,5 +1,5 @@
-# Написать функцию email_parse(<email_address>), которая при помощи регулярного выражения извлекает имя пользователя
-# и почтовый домен из email адреса и возвращает их в виде словаря. Если адрес не валиден,
+# Написать функцию email_parse(<email_address>), которая при помощи регулярного выражения извлекает
+# имя пользователя и почтовый домен из email адреса и возвращает их в виде словаря. Если адрес не валиден,
 # выбросить исключение ValueError.
 #
 # Пример:
@@ -17,4 +17,21 @@
 # имеет ли смысл в данном случае использовать функцию re.compile()?
 
 import re
+
+dict_for_name_and_domen ={}
+
+def email_parse(email_adress):
+    re_email_data = re.compile('.+\@.+\..+')
+    if len(re_email_data.findall(email_adress))>0:
+        dict_for_name_and_domen['username'] = re.compile('.+\@').findall(email_adress)[0][0:-1]
+        dict_for_name_and_domen['domain'] = re.compile('\@.+\..+').findall(email_adress)[0][1:]
+    else:
+        exit('Невалидный email - '+email_adress)
+    print(dict_for_name_and_domen)
+
+
+email_parse('123@yandex.ru')
+email_parse('usrt@gb.ru')
+
+email_parse('user@gmailru')
 
