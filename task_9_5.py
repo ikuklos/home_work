85f4e6fcefaec565f2b651c7c9425a37988b65ac
macbook_pycharm <ikuklos@yandex.ru>
2021-05-11 14:31:55 +0300

    Начато 5 задание 9 урока

--- task_9_5.py
+++ task_9_5.py
@@ -5,3 +5,44 @@
 создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
 
+
+class Stationery:
+    title = 'Канцелярская принадлежность'
+
+    def draw(self):
+        print('Запуск отрисовки')
+
+
+test_stat = Stationery()
+print(test_stat.title)
+test_stat.draw()
+
+
+class Pen(Stationery):
+    def draw(self):
+        print('Запуск отрисовки ручкой')
+
+
+test_pen = Pen()
+print(test_pen.title)
+test_pen.draw()
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print('Запуск отрисовки карандашом')
+
+
+test_pencil = Pencil()
+print(test_pencil.title)
+test_pencil.draw()
+
+
+class Handle(Stationery):
+    def draw(self):
+        print('Запуск отрисовки маркером')
+
+
+test_handle = Handle()
+print(test_handle.title)
+test_handle.draw()
