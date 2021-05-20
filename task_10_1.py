ff469c7653a62ef956dcd699e8668facba7ad10f
macbook_pycharm <ikuklos@yandex.ru>
2021-05-20 19:56:48 +0300

    Закончено 1 задание 10 урока (поправлено оформление)

--- task_10_1.py
+++ task_10_1.py
@@ -14,17 +14,20 @@
 class Matrix:
     def __init__(self, user_input):
         self.matrix = user_input
+
     def __str__(self):
         return '\n'.join([' '.join(map(str, part)) for part in self.matrix])
+
     def __add__(self, other):
-        new_matrix2 = []
+        new_matrix_2 = []
         for string_1, string_2 in zip(self.matrix, other.matrix):
             temp_string = [val_1 + val_2 for val_1, val_2 in zip(string_1, string_2)]
-            new_matrix2.append(temp_string)
-        return '\n'.join([' '.join(map(str, part)) for part in new_matrix2])
+            new_matrix_2.append(temp_string)
+        return '\n'.join([' '.join(map(str, part)) for part in new_matrix_2])
 
-test_matrix_1 = Matrix([[32,22,2], [37,43,13], [51,86,12]])
-test_matrix_2 = Matrix([[1,2], [3,4], [5,6]])
+
+test_matrix_1 = Matrix([[32, 22, 2], [37, 43, 13], [51, 86, 12]])
+test_matrix_2 = Matrix([[1, 2], [3, 4], [5, 6]])
 
 print(test_matrix_1, '\n')
 print(test_matrix_2+test_matrix_1)
